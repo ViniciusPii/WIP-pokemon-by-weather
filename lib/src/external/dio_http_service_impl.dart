d71f4e5be9af3f9688e871b376e33da43b78818f
Vinicius Sales <vinysales@hotmail.com>
2024-04-20 18:03:49 -0300

    feat: Implementando o dio na app

---
+++ lib/src/external/dio_http_service_impl.dart
@@ -0,0 +1,49 @@
+import 'package:dio/dio.dart';
+
+import '/src/core/errors/app_exceptions.dart';
+import '/src/core/external/app_client_response.dart';
+import '/src/core/external/app_log.dart';
+import '/src/core/external/app_log_client_response.dart';
+import '/src/core/external/http_service.dart';
+
+class DioHttpServiceImpl implements HttpService {
+  DioHttpServiceImpl() {
+    _dio = Dio();
+  }
+
+  late final Dio _dio;
+
+  @override
+  Future<AppClientResponse<T>> get<T>(String path) async {
+    try {
+      final response = await _dio.get(path);
+
+      final AppClientResponse<T> appCLientResponse = AppClientResponse(
+        data: response.data,
+        message: response.statusMessage,
+        statusCode: response.statusCode,
+      );
+
+      AppLog.showLog(AppLogClientResponse(
+        data: response.data,
+        message: response.statusMessage,
+        statusCode: response.statusCode,
+        path: response.requestOptions.uri.toString(),
+      ));
+
+      return appCLientResponse;
+    } on DioException catch (e) {
+      AppLog.showLog(AppLogClientResponse(
+        message: e.message,
+        errorType: e.type.toString(),
+        path: e.requestOptions.uri.toString(),
+      ));
+
+      if (e.type == DioExceptionType.connectionError) {
+        throw AppNetworkException();
+      }
+
+      throw AppGenericException();
+    }
+  }
+}
