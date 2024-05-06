e23aeeb032416883838466c4511e50e80e9dcf67
Vinicius Sales <vinysales@hotmail.com>
2024-05-05 21:41:37 -0300

    feat: Testando interceptors

--- lib/src/external/dio_http_service_impl.dart
+++ lib/src/external/dio_http_service_impl.dart
@@ -1,14 +1,14 @@
 import 'package:dio/dio.dart';
+import 'package:pokemon_by_weather/src/external/interceptors/logging_interceptor.dart';
 
 import '/src/core/errors/app_exceptions.dart';
 import '/src/core/external/app_client_response.dart';
-import '/src/core/external/app_log.dart';
-import '/src/core/external/app_log_client_response.dart';
 import '/src/core/external/http_service.dart';
 
 class DioHttpServiceImpl implements HttpService {
   DioHttpServiceImpl() {
     _dio = Dio();
+    _dio.interceptors.add(LoggingInterceptor());
   }
 
   late final Dio _dio;
@@ -24,21 +24,8 @@ class DioHttpServiceImpl implements HttpService {
         statusCode: response.statusCode,
       );
 
-      AppLog.showLog(AppLogClientResponse(
-        data: response.data,
-        message: response.statusMessage,
-        statusCode: response.statusCode,
-        path: response.requestOptions.uri.toString(),
-      ));
-
       return appCLientResponse;
     } on DioException catch (e) {
-      AppLog.showLog(AppLogClientResponse(
-        message: e.message,
-        errorType: e.type.toString(),
-        path: e.requestOptions.uri.toString(),
-      ));
-
       if (e.type == DioExceptionType.connectionError) {
         throw AppNetworkException();
       }
